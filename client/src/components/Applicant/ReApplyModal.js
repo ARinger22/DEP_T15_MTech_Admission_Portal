d4591ce28df8b1aec70249438298ab594edb3822
Tanish Goyal <92181886+Tancoder20@users.noreply.github.com>
2023-03-28 22:51:37 +0530

    updated reapply application section

--- client/src/components/Applicant/ReApplyModal.js
+++ client/src/components/Applicant/ReApplyModal.js
@@ -81,7 +81,7 @@ export default function ReApplyModal(props) {
               re-submission.
             </p>
             <div className="flex items-center justify-end mt-8 text-xs">
-              <Link to={"/re-apply/" + props.application.offering_id}>
+              <Link to={"/Edit Details/" + props.application.offering_id}>
                 <button
                   type="button"
                   className="hover:shadow-lg transition duration-200 border border-red-400 hover:bg-red-600 hover:text-white focus:outline-none w-28 px-4 py-2 font-medium text-red-600 rounded bg-red-50"
